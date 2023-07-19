14e9037102b5eb5dc85fb2bd36702dd057cabe12
Artom <artomfj@gmail.com>
2023-07-19 15:20:06 -0300

    feat: Add User test

---
+++ app/tests/UserTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests;
+
+use App\Models\User;
+
+class UserTest extends TestCase
+{
+    // Testando o método que verifica se o usuário é pessoa física ou lojista(jurídica)
+    public function testIsNaturalPerson()
+    {
+        // Cenário 1: O usuário é pessoa física
+        $user = new User();
+        $user->type = 1;
+        $this->assertTrue($user->isNaturalPerson());
+
+        // Cenário 2: O usuário não é pessoa física
+        $user->type = 2;
+        $this->assertFalse($user->isNaturalPerson());
+    }
+}
